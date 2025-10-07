5a408cec0edf29b4ff0933d4f21c2aceff1f70b2
Sebastian Jimenez <tatansebas.29@gmail.com>
2025-10-06 19:13:02 -0500

    proyecto base

---
+++ src/main/java/io/bootify/consultorio/ConsultorioApplication.java
@@ -0,0 +1,14 @@
+package io.bootify.consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConsultorioApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ConsultorioApplication.class, args);
+    }
+
+}
